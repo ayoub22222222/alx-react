f6f21fc6cfefceaee370bf23c9d4ac7929e0cf81
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-09-16 19:56:28 +0200

    task number 1

---
+++ 0x02-react_props/task_1/dashboard/config/webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "webpack output",
+			template: './dist/index.html',
+		}),
+	],
+
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../dist')
+	},
+
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, '../dist'),
+		},
+		open: true,
+		hot: true,
+		port: 3000,
+	},
+
+	module: {
+
+		rules: [
+			{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			}
+
+		},
+
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader'],
+		},
+
+		{
+			test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+			use: {
+				loader: 'url-loader',
+				options: {
+					limit: 8192,
+					name: '[name].[hash:8].[ext]',
+					outputPath: 'images',
+				}
+			}
+		}
+		]
+	},
+	mode: 'development',
+}
---
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,19 @@
+import './Login.css';
+import logo from '../assets/logo.jpg';
+import React from 'react';
+
+
+function Login() {
+  return (
+      <div className="App-body">
+	  <p>Login to access the full dashboard</p>
+	  <label for="email">Email: </label>
+	  <input type="email" id="email" name="email" required/>
+	  <label for="password"> Password: </label>
+	  <input type="password"/>
+	  <button>OK</button>
+      </div>
+  );
+}
+
+export default Login;
