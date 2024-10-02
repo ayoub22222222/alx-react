3161a8bf71b47e38ea693d042b86d893f65bd831
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-10-02 17:24:49 +0200

    update on task 2

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,15 +1,18 @@
-import Reatc from 'react';
+import React from 'react';
+import './Notification.css';
 
 
 
 
-
-function NotificationItem() {
+function NotificationItem({type, html, value}) {
 	return (
 		<>
-			<li></li>
+			{type && value ? <li data-notification-type={type}></li> : null}
+			{html ? (
+				<li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+			) : null}
 		</>
-	)
+	);
 };
 
 export default NotificationItem;
--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './Notifications.css';
 import { getLatestNotification } from '../utils/utils';
 import close_image  from '../assets/close_image.png';
+import NotificationItem from './NotificationItem';
 
-function Notifucation() {
+function Notification() {
 	return (<div className="Notifications">
-			<p>Here is the list of notifications</p>
+			<p>
+				Here is the list of notifications
+			</p>
 		        <button  arial-label="Close">
 				<img src={ close_image } alt="close emoji" width="5" height="7"/>
 		        </button>
 		        <ul>
-				<li data-priority="high">New course available</li>
-				<li data-priority="medium">New resume available</li>
-				<li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+				<NotificationItem type='defaut' value='New course availabale' />
+				<NotificationItem type='urgent' value='New resume avaiable' />
+				<NotificationItem type='urgent' html={getLatestNotification()} />
 		        </ul>
 		</div>);
 }
 
-export default Notifucation;
+export default Notification;
