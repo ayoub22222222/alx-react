ba204fc615bc5c6f3ecd842fdbc4398701723878
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-10-02 17:31:47 +0200

    task 2 another update

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -7,7 +7,7 @@ import './Notification.css';
 function NotificationItem({type, html, value}) {
 	return (
 		<>
-			{type && value ? <li data-notification-type={type}></li> : null}
+			{type && value ? <li data-notification-type={type}>(value)</li> : null}
 			{html ? (
 				<li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
 			) : null}
