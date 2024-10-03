15a3317e0b8836e642b316e669d752a6e838124c
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-10-03 22:13:53 +0200

    update on task 4

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function CourseListRow({isHeader = false, textFirstCell, textSecondCell = null}) {
+	return (
+		<>
+			
+		</>
+	);
+};
+
+export default CourseListRow; 
